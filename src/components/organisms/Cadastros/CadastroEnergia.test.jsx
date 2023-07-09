8022a4c5338de18c6f03a7328cf0cb5cd535815c
Neli33 <118769908+Neli33@users.noreply.github.com>
2023-07-09 23:17:02 +0300

    Configuração e test de Navbar

---
+++ src/components/organisms/Cadastros/CadastroEnergia.test.jsx
@@ -0,0 +1,13 @@
+//Teste:
+
+//se o componente é renderizado corretamente: com título “Cadastro de Energia Gerada”
+//se algum camplo obrigatório não estiver preenchido, ao clicar em “Salvar”, não deverá chamar a função de abrir/fechar formulário
+//se preencher todos os campos obrigatórios, ao clicar em “Salvar”, deverá chamar a função de abrir/fechar formulário com a opção correta
+//se preencher todos os campos obrigatórios, ao clicar em “Salvar”, deverá limpar as informações do formulário
+//ao clicar no checkbox seu estado deve ser alterado entre checado e não checado
+//quando não houver unidade selecionada:
+//o formulário deverá iniciar com os campos vazios
+//ao preencher todos os campos obrigatórios, ao clicar em “Salvar”, deverá acontecer uma requisição do tipo POST na api
+//quando houver unidade selecionada
+//o formulário deverá iniciar com os campos preenchidos com os valores da unidade selecionada
+//ao preencher todos os campos obrigatórios, ao clicar em “Salvar”, deverá acontecer uma requisição do tipo PUT na api
---
+++ src/components/organisms/UnidadesLista/ListaUnidades.test.jsx
@@ -0,0 +1,13 @@
+//Teste:
+
+//se o componente é renderizado corretamente: com título “Unidades”
+//ao entrar na página deve renderiza a lista de unidades
+//ao clicar no botão “Nova unidade” deve renderizar o cadastro de unidade
+
+//ex4
+//se o componente é renderizado corretamente: com título “Lista de Unidades”
+//se a tabela é renderizada com o cabeçalho correto
+//se a primeira linha da tabela é rendeizada corretamente (criar mock da API)
+//testar comportamento do botão “Editar”
+//testar comportamento do botão “Remover”
+//testar comportamento do botão “Nova Unidade”
